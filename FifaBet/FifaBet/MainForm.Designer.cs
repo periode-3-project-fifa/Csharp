5b10872d614bd2eaa34f8e6e5be4ee30ac4d9ffa
Bas <supersiku1234@hotmail.nl>
2019-04-15 10:29:56 +0200

    Form Csharp
    Form betters csharp

---
+++ FifaBet/FifaBet/MainForm.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace FifaBet
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelCredits = new System.Windows.Forms.Label();
+            this.comboBoxGames = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDownBet = new System.Windows.Forms.NumericUpDown();
+            this.labelBetCredits = new System.Windows.Forms.Label();
+            this.checkBoxTeamOne = new System.Windows.Forms.CheckBox();
+            this.checkBoxTeamTwo = new System.Windows.Forms.CheckBox();
+            this.groupBoxBetTeam = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.labelTeamOne = new System.Windows.Forms.Label();
+            this.labelTeamTwo = new System.Windows.Forms.Label();
+            this.groupBoxRightScore = new System.Windows.Forms.GroupBox();
+            this.buttonBet = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBet)).BeginInit();
+            this.groupBoxBetTeam.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.groupBoxRightScore.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelCredits
+            // 
+            this.labelCredits.AutoSize = true;
+            this.labelCredits.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCredits.Location = new System.Drawing.Point(358, 9);
+            this.labelCredits.Name = "labelCredits";
+            this.labelCredits.Size = new System.Drawing.Size(59, 20);
+            this.labelCredits.TabIndex = 0;
+            this.labelCredits.Text = "Credits";
+            // 
+            // comboBoxGames
+            // 
+            this.comboBoxGames.FormattingEnabled = true;
+            this.comboBoxGames.Location = new System.Drawing.Point(108, 76);
+            this.comboBoxGames.Name = "comboBoxGames";
+            this.comboBoxGames.Size = new System.Drawing.Size(191, 21);
+            this.comboBoxGames.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(172, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Inzet:";
+            // 
+            // numericUpDownBet
+            // 
+            this.numericUpDownBet.Location = new System.Drawing.Point(126, 171);
+            this.numericUpDownBet.Name = "numericUpDownBet";
+            this.numericUpDownBet.Size = new System.Drawing.Size(54, 20);
+            this.numericUpDownBet.TabIndex = 3;
+            // 
+            // labelBetCredits
+            // 
+            this.labelBetCredits.AutoSize = true;
+            this.labelBetCredits.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBetCredits.Location = new System.Drawing.Point(200, 171);
+            this.labelBetCredits.Name = "labelBetCredits";
+            this.labelBetCredits.Size = new System.Drawing.Size(59, 20);
+            this.labelBetCredits.TabIndex = 4;
+            this.labelBetCredits.Text = "Credits";
+            // 
+            // checkBoxTeamOne
+            // 
+            this.checkBoxTeamOne.AutoSize = true;
+            this.checkBoxTeamOne.Location = new System.Drawing.Point(6, 30);
+            this.checkBoxTeamOne.Name = "checkBoxTeamOne";
+            this.checkBoxTeamOne.Size = new System.Drawing.Size(62, 17);
+            this.checkBoxTeamOne.TabIndex = 5;
+            this.checkBoxTeamOne.Text = "Team 1";
+            this.checkBoxTeamOne.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxTeamTwo
+            // 
+            this.checkBoxTeamTwo.AutoSize = true;
+            this.checkBoxTeamTwo.Location = new System.Drawing.Point(123, 30);
+            this.checkBoxTeamTwo.Name = "checkBoxTeamTwo";
+            this.checkBoxTeamTwo.Size = new System.Drawing.Size(62, 17);
+            this.checkBoxTeamTwo.TabIndex = 6;
+            this.checkBoxTeamTwo.Text = "Team 2";
+            this.checkBoxTeamTwo.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxBetTeam
+            // 
+            this.groupBoxBetTeam.Controls.Add(this.checkBoxTeamOne);
+            this.groupBoxBetTeam.Controls.Add(this.checkBoxTeamTwo);
+            this.groupBoxBetTeam.Location = new System.Drawing.Point(108, 219);
+            this.groupBoxBetTeam.Name = "groupBoxBetTeam";
+            this.groupBoxBetTeam.Size = new System.Drawing.Size(191, 67);
+            this.groupBoxBetTeam.TabIndex = 7;
+            this.groupBoxBetTeam.TabStop = false;
+            this.groupBoxBetTeam.Text = "Winner";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(6, 58);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(72, 20);
+            this.numericUpDown1.TabIndex = 8;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(123, 58);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(72, 20);
+            this.numericUpDown2.TabIndex = 9;
+            // 
+            // labelTeamOne
+            // 
+            this.labelTeamOne.AutoSize = true;
+            this.labelTeamOne.Location = new System.Drawing.Point(6, 30);
+            this.labelTeamOne.Name = "labelTeamOne";
+            this.labelTeamOne.Size = new System.Drawing.Size(43, 13);
+            this.labelTeamOne.TabIndex = 10;
+            this.labelTeamOne.Text = "Team 1";
+            // 
+            // labelTeamTwo
+            // 
+            this.labelTeamTwo.AutoSize = true;
+            this.labelTeamTwo.Location = new System.Drawing.Point(120, 30);
+            this.labelTeamTwo.Name = "labelTeamTwo";
+            this.labelTeamTwo.Size = new System.Drawing.Size(43, 13);
+            this.labelTeamTwo.TabIndex = 11;
+            this.labelTeamTwo.Text = "Team 2";
+            // 
+            // groupBoxRightScore
+            // 
+            this.groupBoxRightScore.Controls.Add(this.numericUpDown1);
+            this.groupBoxRightScore.Controls.Add(this.labelTeamTwo);
+            this.groupBoxRightScore.Controls.Add(this.numericUpDown2);
+            this.groupBoxRightScore.Controls.Add(this.labelTeamOne);
+            this.groupBoxRightScore.Location = new System.Drawing.Point(108, 316);
+            this.groupBoxRightScore.Name = "groupBoxRightScore";
+            this.groupBoxRightScore.Size = new System.Drawing.Size(200, 100);
+            this.groupBoxRightScore.TabIndex = 12;
+            this.groupBoxRightScore.TabStop = false;
+            this.groupBoxRightScore.Text = "Juiste uitslag";
+            // 
+            // buttonBet
+            // 
+            this.buttonBet.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBet.Location = new System.Drawing.Point(108, 459);
+            this.buttonBet.Name = "buttonBet";
+            this.buttonBet.Size = new System.Drawing.Size(200, 42);
+            this.buttonBet.TabIndex = 13;
+            this.buttonBet.Text = "Inzetten";
+            this.buttonBet.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 550);
+            this.Controls.Add(this.buttonBet);
+            this.Controls.Add(this.groupBoxRightScore);
+            this.Controls.Add(this.groupBoxBetTeam);
+            this.Controls.Add(this.labelBetCredits);
+            this.Controls.Add(this.numericUpDownBet);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxGames);
+            this.Controls.Add(this.labelCredits);
+            this.Name = "MainForm";
+            this.Text = "BetForm";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBet)).EndInit();
+            this.groupBoxBetTeam.ResumeLayout(false);
+            this.groupBoxBetTeam.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.groupBoxRightScore.ResumeLayout(false);
+            this.groupBoxRightScore.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelCredits;
+        private System.Windows.Forms.ComboBox comboBoxGames;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDownBet;
+        private System.Windows.Forms.Label labelBetCredits;
+        private System.Windows.Forms.CheckBox checkBoxTeamOne;
+        private System.Windows.Forms.CheckBox checkBoxTeamTwo;
+        private System.Windows.Forms.GroupBox groupBoxBetTeam;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label labelTeamOne;
+        private System.Windows.Forms.Label labelTeamTwo;
+        private System.Windows.Forms.GroupBox groupBoxRightScore;
+        private System.Windows.Forms.Button buttonBet;
+    }
+}
+
